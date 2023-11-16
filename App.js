57eb1d4157474113eb2c903f1773034dc9ae5d96
yulimaurya22 <yulimaurya@gmail.com>
2023-11-16 21:41:30 +0700

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from"@react-navigation/native-stack"
+import { Login , Signup , Welcome } from "./screens"
 
+
+const stack = createNativeStackNavigator()
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    <NavigationContainer>
+      <stack.Navigator
+        initialRouteName='Welcome'>
+
+          <stack.Screen
+            name="Welcome"
+            component={Welcome}
+            options={{
+              headerShown:false
+            }}          
+          />
+
+          <stack.Screen
+            name="Login"
+            component={Login}
+            options={{
+              headerShown:false
+            }}       
+            />
+            
+            <stack.Screen
+            name="Signup"
+            component={Signup}
+            options={{
+              headerShown:false
+            }}   
+          />
+      </stack.Navigator>
+    </NavigationContainer>
+      );
+    }
+    
+  
---
+++ screens/Home.js
@@ -0,0 +1,12 @@
+import { SafeAreaView, Text, View } from 'react-native'
+import React from 'react'
+import COLORS from '../constants/colors'
+
+
+const Home = ({ navgation }) => {
+    return (
+    <View></View>
+   
+    )
+}
+export default Home
